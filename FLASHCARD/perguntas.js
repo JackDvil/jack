dfa04e6190ea814f996c35f360798f4da26de217
JackDvil <amystronofc@gmail.com>
2024-10-22 11:58:38 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'SS1',
+    'Existem quantos estagios do SS1?',
+    'Coexistem três fases do SS1'
+)
+
+criaCartao(
+    'Celulas SR',
+    'Por que Sayajins que nasce em ambientes mais calmos desenvolvem melhor celulas SR?',
+    'Pois o ambiente calmo contribui a um processo mais eficiente da produção dessa celulas, do que um agitado'
+)
+
+criaCartao(
+    'Pedido',
+    'Por que os sheilongs não podem realizar alguns pedidos',
+    'Pois eles só podem realizar desejos que são abaixo do nivel de seu criador'
+)
+
+criaCartao(
+    'top 2',
+    'Quem é mais forte daishinka, ou os guardas do zeno-sama',
+    'Os guardas, pois diferente dos anjos eles são feitos e ter permisão pra lutar a serio'
+)
